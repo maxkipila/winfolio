9cae39d2ed5092abb8e43d468168072077bbf130
Maxim Kipila <max.kipila@gmail.com>
2025-06-07 18:51:55 +0200

    Fix product gallery layout

--- resources/js/Components/product/ProductGalery.tsx
+++ resources/js/Components/product/ProductGalery.tsx
@@ -10,15 +10,15 @@ function ProductGalery(props: Props) {
     let [galeryIndex, setGaleryIndex] = useState(0)
     return (
         <div className='flex gap-24px mob:flex-col mob:mt-24px'>
-            <div className='border-2 border-black w-full max-w-[590px] max-h-[444px] overflow-hidden'>
-                <Img className='w-full object-cover' src={product?.images[galeryIndex]} />
+            <div className='border-2 border-black w-full max-w-[590px] h-[444px] overflow-hidden flex items-center justify-center'>
+                <Img className='w-full h-full object-contain object-center' src={product?.images[galeryIndex]} />
             </div>
             {
-                product?.images?.length > 0 &&
+                product?.images?.length > 1 &&
                 <div className='flex flex-col gap-24px mob:flex-row mob:overflow-x-auto max-h-[444px] overflow-auto tagscrollbar'>
                     {
                         product?.images?.map((im, i) =>
-                            <Img onClick={()=>{setGaleryIndex(i)}} className='max-w-[200px] border-2 border-black cursor-pointer' src={im} />
+                            <Img onClick={() => { setGaleryIndex(i) }} className={`max-w-[200px] border-2 cursor-pointer ${galeryIndex === i ? 'border-app-button-dark bg-app-button/50' : 'border-black'}`} src={im} />
                         )
                     }
 
